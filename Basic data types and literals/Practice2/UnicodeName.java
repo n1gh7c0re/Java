d86e7e36df32fb68230c74182f8f91061e057fff
Sergey <159152648+n1gh7c0re@users.noreply.github.com>
2024-09-09 00:37:30 +0300

    Add files via upload

---
+++ Basic data types and literals/Practice2/UnicodeName.java
@@ -0,0 +1,17 @@
+public class UnicodeName {
+    public static void main(String[] args){
+
+        System.out.println("\u0421\u0435\u0440\u0433\u0435\u0439");
+    }
+}
+
+/*
+
+\u0421 - С
+\u0435 - е
+\u0440 - р
+\u0433 - г
+\u0435 - е
+\u0439 - й
+
+*/
